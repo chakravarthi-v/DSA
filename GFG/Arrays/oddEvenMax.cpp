57d5673ffe0f5ee6dc2173ad116b008ab007ab8a
chakravarthi-v <chakravarthiviswanath@gmail.com>
2021-10-23 17:27:41 +0530

    arrays H

---
+++ GFG/Arrays/oddEvenMax.cpp
@@ -0,0 +1,53 @@
+//Initial Template for C++
+#include<iostream> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Complete this function
+    //Function to find the length of longest subarray of even and odd numbers.
+    int maxEvenOdd(int arr[], int n) 
+    { 
+       //Your code here
+       int maxS=0,diff=0;
+       for(int i=1;i<n;i++){
+           if((arr[i]+arr[i-1])%2!=0){
+               maxS++;
+           }
+           else{
+               maxS=0;
+           }
+           diff=max(diff,maxS);
+       }
+       return diff+1;
+    } 
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int testcases;
+    //testcases
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        //size of array
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        
+        //inserting elements in the array
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        //calling function
+        cout  << ob.maxEvenOdd(arr, sizeOfArray)<<endl; 
+    }
+   return 0; 
+} 
+  // } Driver Code Ends
