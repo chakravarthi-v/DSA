fc928b2baf1f8b8f08b01eb20afad7b6a6e7ad67
chakravarthi-v <chakravarthiviswanath@gmail.com>
2021-11-05 17:47:38 +0530

    matrix

---
+++ GFG/Matrix/makeMatrixBeautifull.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int> > matrix, int n)
+    {
+        // code here 
+        int maxS=INT_MIN,tot=0;
+        for(int i=0;i<n;i++){
+            int col=0,row=0;
+            for(int j=0;j<n;j++){
+                col+=matrix[i][j];
+                tot+=matrix[i][j];
+                row+=matrix[j][i];
+            }
+            maxS=max(maxS,max(col,row));
+        }
+        int res=(n*maxS)-tot;
+        return res;
+        
+    } 
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix (n,vector<int>(n));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>matrix[i][j];
+        Solution ob;
+        cout << ob.findMinOpeartion(matrix, n) << endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
